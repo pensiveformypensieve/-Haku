90832ddc381bf9c755ffce101aa4e6c1eabc26f8
Fiona <6686045-ftjy@users.noreply.gitlab.com>
2021-10-24 18:36:38 +0800

    javascript journey starts....

---
+++ freelancer-rates/freelancer-rates.js
@@ -0,0 +1,80 @@
+// @ts-check
+//
+// ☝🏽 The line above enables type checking for this file. Various IDEs interpret
+// the @ts-check directive. It will give you helpful autocompletion on the web
+// and supported IDEs when implementing this exercise. You don't need to
+// understand types, JSDoc, or TypeScript in order to complete this JavaScript
+// exercise, and can completely ignore this comment block and directive.
+
+// 👋🏽 Hi again!
+//
+// A quick reminder about exercise stubs:
+//
+// 💡 You're allowed to completely clear any stub before you get started. Often
+// we recommend using the stub, because they are already set-up correctly to
+// work with the tests, which you can find in ./freelancer-rates.spec.js.
+//
+// 💡 You don't need to write JSDoc comment blocks yourself; it is not expected
+// in idiomatic JavaScript, but some companies and style-guides do enforce them.
+//
+// Get those rates calculated!
+
+export const NUMBER_OF_DAYS_PER_MONTH = 22;
+
+/**
+ * The day rate, given a rate per hour
+ *
+ * @param {number} ratePerHour
+ * @returns {number} the rate per day
+ */
+export function dayRate(ratePerHour) {
+  // throw new Error('Implement the dayRate function');
+  let dayRate = ratePerHour * 8;
+
+  return dayRate;
+}
+
+/**
+ * Calculates the rate per month
+ *
+ * @param {number} ratePerHour
+ * @param {number} discount for example 20% written as 0.2
+ * @returns {number} the rounded up monthly rate
+ */
+export function monthRate(ratePerHour, discount) {
+  // throw new Error('Implement the monthRate function');
+  let monthRate = (dayRate(ratePerHour)*22);
+
+  return Math.ceil(applyDiscount(monthRate, discount));
+}
+
+/**
+ * Calculates the number of days in a budget, rounded down
+ *
+ * @param {number} budget the total budget
+ * @param {number} ratePerHour the rate per hour
+ * @param {number} discount to apply, example 20% written as 0.2
+ * @returns {number} the number of days
+ */
+export function daysInBudget(budget, ratePerHour, discount) {
+  // throw new Error('Implement the daysInBudget function');
+
+  let daysInBudget= Math.floor((budget/(monthRate(ratePerHour, discount)))*22);
+
+  return daysInBudget;
+}
+
+/**
+ * Applies a discount to the value
+ *
+ * @param {number} value
+ * @param {number} percentage for example 20% written as 0.2
+ * @returns {number} the discounted value
+ */
+function applyDiscount(value, percentage) {
+  // throw new Error('Implement the applyDiscount function');
+  let discountedValue = value * (1-percentage);
+
+  return discountedValue;
+}
+
